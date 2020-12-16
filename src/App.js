fb6afd0c753dc545f2bca942056cc181debe2087
gelenek <abdullahdenizfr@gmail.com>
2020-12-16 14:06:24 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  
+  Link,
+
+} from 'react-router-dom';
+// import Discover from './components/Popular';
+// import DiscoverBattle from './components/DiscoverBattle';
+// import MyList from './components/MyList';
+// import Popular from './components/Discover';
+// import PopularBattle from './components/PopularBattle';
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Hello World !</h1>
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li><Link to="/">About</Link></li>
+              <li><Link to="/battle/">Experince</Link></li>
+              <li><Link to="/popular/">Education</Link></li>
+            </ul>
+          </nav>
+          {/* <Switch>
+            <Route path="/">
+              <Discover />
+            </Route>
+            <Route path="/battle/">
+              <DiscoverBattle />
+            </Route>
+            <Route path="/popular/">
+              <MyList />
+            </Route>
+            <Route path="/popularbattle/">
+              <Popular />
+            </Route>
+            <Route path="/mylist/">
+              <PopularBattle />
+            </Route>
+          </Switch> */}
+        </div>
+      </Router>
     );
   }
 }
-
 export default App;
