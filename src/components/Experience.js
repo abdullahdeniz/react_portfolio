218152a115884ad79f68a0b51a6cba8cf6695d47
gelenek <abdullahdenizfr@gmail.com>
2020-12-20 17:25:22 +0100

    initial

---
+++ src/components/Experience.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Router } from 'react-router-dom'
+
+
+class Experience extends React.Component {
+    render () {
+        return (
+            <Router>
+                <div>
+                    <p>Mon premier experience en Enseignement en Albanie l'année du 2013 jusqu'a 2015 </p>
+                </div>
+            </Router>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import About from './components/About'
+import Experince from './components/Experience'
 
 ReactDOM.render(
   <React.StrictMode>
