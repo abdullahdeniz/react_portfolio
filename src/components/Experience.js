9137e4cf09bc379f00d000b96129d8154552ddda
gelenek <abdullahdenizfr@gmail.com>
2020-12-22 11:33:19 +0100

    initial

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -9,7 +9,13 @@ class Experience extends React.Component {
                 <div>
                     <p>Mon premier experience en Enseignement en Albanie l'année du 2013 jusqu'a 2015 </p>
                 </div>
+                <div>
+                    <p>yes</p>
+                </div>
             </Router>
+            
+                
+           
         )
     }
 }
