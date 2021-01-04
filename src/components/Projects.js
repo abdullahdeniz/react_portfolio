caf8f0d63ea785b7a03430d858fbb27c71a88627
gelenek <abdullahdenizfr@gmail.com>
2021-01-04 23:11:22 +0100

    initial

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,10 +2,17 @@ import React from 'react';
 import { Router } from 'react-router-dom'
 
 class Projects extends React.Component{
-    render () {
-        return {
+    render (){
+        return(
+            <Router>
+                <div>   
+                    <p>Les cours </p>
+                    <p>Les Formations</p>
+                    <p>Les Voyages</p>
+                </div>
+            </Router>
+        )
         
-        }
     }
 };
 
